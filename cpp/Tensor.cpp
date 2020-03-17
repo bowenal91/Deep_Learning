530bc0cf2bbb9d0201a8c5c7670be9fe0324ada1
Alec Bowen <bowenal91@gmail.com>
2020-03-17 11:02:13 -0700

    completed Tensor

--- cpp/Tensor.cpp
+++ cpp/Tensor.cpp
@@ -1,5 +1,37 @@
 #include "Tensor.hpp"
 
-Tensor::Tensor() {
+using namespace std;
+Tensor::Tensor(vector<int> input_shape) {
+    int local_size = 1;
+    shape.clear();
+    vals.clear();
+    dimensions = input_shape.size();
+    for (int i=0;i<dimensions;i++) {
+        shape.push_back(input_shape[i]);
+        size *= input_shape[i];
+    }
+    size = local_size;
+    vals.resize(size,0);
     return;
+}
+
+int Tensor::map_id(vector<int> id) {
+    int prefactor = 1;
+    int output = 0;
+    for (int i; i<dimensions; i++) {
+        output += id[i]*prefactor;
+        prefactor *= shape[i];
+    }
+    return output;
+}
+
+void Tensor::set_value(vector<int> id, double, val) {
+    int i = map_id(id);
+    vals[i] = val;
+    return;
+}
+
+double Tensor::get_value(vector<int> id) {
+    int i = map_id(id);
+    return vals[i];
 }
--- cpp/Tensor.hpp
+++ cpp/Tensor.hpp
@@ -1,8 +1,13 @@
 class Tensor {
-    std::vector<int> size;
+    std::vector<int> shape;
     std::vector<double> vals;
+    int size;
+    int dimensions;
+    int map_id(std::vector<int> ids);
     public:
-        Tensor();
+        Tensor(std::vector<int> input_shape);
+        void set_value(std::vector<int> id, double val);
+        double get_value(std::vector<int> id);
 };
 
 
