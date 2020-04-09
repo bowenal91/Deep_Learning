9e94bec31d14b45bb3302ac64e873fd917f569bd
Alec Bowen <bowenal91@gmail.com>
2020-04-08 21:31:16 -0700

    Updated python module

--- cpp/nn.cpp
+++ cpp/nn.cpp
@@ -1,26 +1,24 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
+#include "pybind11/operators.h"
 #include "Tensor.cpp"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(nn, m) {
     py::class_<Tensor>(m, "Tensor")
+        .def(py::init<>())
         .def(py::init< vector<int> >())
-        .def("set_value", &Tensor::set_value)
-        .def("get_value", &Tensor::get_value);
+        .def("set_value", (void (Tensor::*) (const vector<int> &, double)) &Tensor::set_value, "Get by indices")
+        .def("set_value", (void (Tensor::*) (const int, double)) &Tensor::set_value, "Get by absolute index")
+        .def("get_value", (double (Tensor::*) (const vector<int> &)) &Tensor::get_value)
+        .def("get_value", (double (Tensor::*) (const int)) &Tensor::get_value)
+        .def("resize", &Tensor::resize)
+        .def(py::self * py::self)
+        .def(py::self ^ py::self)
+        .def(py::self + py::self)
+        .def(py::self - py::self)
+        .def("subset_mult", &Tensor::subset_mult);
+
 }
 
-/*
-PYBIND11_MODULE(example, m) {
-    py::class_<Pet>(m, "Pet")
-        .def(py::init<const std::string &>())
-        .def("setName", &Pet::setName)
-        .def("getName", &Pet::getName)
-        .def("__repr__",
-                [](const Pet &a) {
-                    return "Pet named " + a.name + "";
-                }
-        );
-}
-*/
