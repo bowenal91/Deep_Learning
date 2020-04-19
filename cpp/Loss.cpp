fdc46bd78dfdc559fd2ccbff24c13445e44ba8e7
Alec Bowen <bowenal91@gmail.com>
2020-04-19 01:15:13 -0700

    Added loss functions

---
+++ cpp/Loss.cpp
@@ -0,0 +1,81 @@
+#include "Loss.hpp"
+#include <math.h>
+#include <assert.h>
+
+using namespace std;
+
+double Loss::calculate_loss(Tensor &y_pred, Tensor &y_label) {
+    assert(y_pred.size == y_label.size);
+    assert(y_pred.rank == y_label.rank);
+    for (int i=0;i<y_pred.rank;i++) {
+        assert(y_pred.shape[i] == y_label.shape[i]);
+    }
+
+    int batch_size = y_pred.shape[0];
+    double prefactor = 1.0/double(batch_size);
+    double error = 0.0;
+    error_deriv.resize(y_pred.shape);
+
+    for (int i=0;i<y_pred.size;i++) {
+        error += point_wise_func(y_pred.get_value(i), y_label.get_value(i));
+        error_deriv.set_value(i, prefactor*deriv(i, y_pred.get_value(i), y_label.get_value(i)));
+    }
+
+    return prefactor*error;
+}
+
+Tensor Loss::back_propagate() {
+    return error_deriv;
+}
+
+double MSE::point_wise_func(double x_pred, double x_label) {
+    return (x_pred-x_label)*(x_pred-x_label);
+}
+
+double MSE::deriv(double x_pred, double x_label) {
+    return 2*(x_pred-x_label);
+}   
+
+double MAE::point_wise_func(double x_pred, double x_label) {
+    return abs(x_pred-x_label);
+}
+
+double MAE::deriv(double x_pred, double x_label) {
+    if (x_pred > x_label) {
+        return 1.0;
+    } else {
+        return -1.0;
+    }
+}
+
+double Binary_Cross_Entropy::point_wise_func(double x_pred, double x_label) {
+    if (x_label > 0.5) {
+        return log(x_pred);
+    } else {
+        return log(1.0-x_pred);
+    }
+}
+
+double Binary_Cross_Entropy::deriv(double x_pred, double x_label) {
+    if (x_label > 0.5) {
+        return 1.0/x_pred;
+    } else {
+        return -1.0/(1.0-x_pred);
+    }
+}
+
+double Categorical_Cross_Entropy::point_wise_func(double x_pred, double x_label) {
+    if (x_label > 0.5) {
+        return log(x_pred);
+    } else {
+        return 0.0;
+    }
+}
+
+double Categorical_Cross_Entropy::deriv(double x_pred, double x_label) {
+    if (x_label > 0.5) {
+        return 1.0/x_pred;
+    } else {
+        return 0.0;
+    }
+}
--- cpp/Loss.hpp
+++ cpp/Loss.hpp
@@ -1,20 +1,48 @@
+#ifndef LOSS_H
+#define LOSS_H
+
 #include <vector>
 #include "Tensor.cpp"
 
 class Loss {
-    int batchSize;
-    Loss_Function *evaluator;
+    Tensor error_deriv;
     public:
-        Loss(std::string& name);
-        
+        Loss() {};
+        double calculate_loss(Tensor &y_pred, &Tensor y_label);
+        virtual double point_wise_func(double x_pred, double x_label);
+        virtual double deriv(double x_pred, double x_label);
+        Tensor back_propagate();
 };
 
-class Loss_Function {
-     
+class MSE : public Loss {
     public:
-        Loss_Function();
-        virtual void evaluate(double y, double y_hat);
-        double evaluate_loss();
-        virtual void add_deriv(double y, double y_hat);
-        double evaluate_derivative();
+        MSE() : Loss() {};
+        double point_wise_func(double x_pred, double x_label) override;
+        double deriv(double x_pred, double x_label) override;
+
+};
+
+class MAE : public Loss {
+    public:
+        MAE() : Loss() {};
+        double point_wise_func(double x_pred, double x_label) override;
+        double deriv(double x_pred, double x_label) override;
 }
+
+class Binary_Cross_Entropy : public Loss {
+    public:
+        Binary_Cross_Entropy() : Loss() {};
+        double point_wise_func(double x_pred, double x_label) override;
+        double deriv(double x_pred, double x_label) override;
+
+};
+
+class Categorical_Cross_Entropy : public Loss {
+     public:
+        Categorical_Cross_Entropy() : Loss() {};
+        double point_wise_func(double x_pred, double x_label) override;
+        double deriv(double x_pred, double x_label) override;
+   
+}
+
+#endif
