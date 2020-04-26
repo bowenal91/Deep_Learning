682008350180df2c9dc50b7b96814a7c72d4ec6c
Alec Bowen <bowenal91@gmail.com>
2020-04-26 15:38:11 -0700

    training and prediction work properly

--- cpp/Layer.hpp
+++ cpp/Layer.hpp
@@ -9,13 +9,14 @@ class Layer {
     public:
         Layer() {};
         virtual std::vector<int> get_output_shape() {};
-        virtual void init_layer(const std::vector<int> &shape) {};
         virtual Tensor evaluate(Tensor& input) {};
         virtual std::vector<Tensor> evaluate(std::vector<Tensor> &input) {};
         virtual Tensor back_propagate(Tensor &forward, Tensor &backward) {};
         virtual std::vector<Tensor> back_propagate(std::vector<Tensor> &forward, std::vector<Tensor> &backward) {};
-        virtual void update_weights(std::vector<Tensor> &forward, std::vector<Tensor> &backward, int rate) {};
-        virtual std::vector<Tensor> update_propagate(std::vector<Tensor> &forward, std::vector<Tensor> &backward, int rate) {}; 
+        virtual void update_weights(std::vector<Tensor> &forward, std::vector<Tensor> &backward, double rate) {};
+        virtual std::vector<Tensor> update_propagate(std::vector<Tensor> &forward, std::vector<Tensor> &backward, double rate) {};
+        virtual std::vector<Tensor> get_weights() {};
+        virtual void set_weights(std::vector<Tensor> &data) {};
 };
 
 #endif
